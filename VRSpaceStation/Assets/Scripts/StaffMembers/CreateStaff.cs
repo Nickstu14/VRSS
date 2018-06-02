af61c354870f4771558305cd957078d907b86eb9
Nickstu14 <nickstu14@hotmail.co.uk>
2018-06-02 19:50:59 +0000

    Started work on crew creation

---
+++ VRSpaceStation/Assets/Scripts/StaffMembers/CreateStaff.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Staff
+{
+   public enum Gender
+    {
+        MALE,
+        FEMALE
+    }
+
+    public struct StaffMember
+    {
+        public string m_ForName;
+        public string m_Surename;
+        public string m_JobTitle;
+        public Gender m_Gender; // ture female, false male
+    }
+
+    public class CreateStaff : LoadFromText
+    {
+        public List<StaffMember> m_SMember;
+
+        // Use this for initialization
+        void Start()
+        {
+            m_SMember = new List<StaffMember>();
+
+            for (int i = 0;i <10;i++)
+            {
+                SetStaffValues(Random.Range(0, 1));
+            
+            }
+        }
+
+        void SetStaffValues(int Val)
+        {
+
+        }
+
+        string GetStaffForeName(int _Val)
+        {
+            return m_SMember[_Val].m_ForName;
+        }
+        string GetSurName(int _Val)
+        {
+            return m_SMember[_Val].m_Surename;
+        }
+        string GetJob(int _Val)
+        {
+            return m_SMember[_Val].m_JobTitle;
+        }
+    }
+}
---
+++ VRSpaceStation/Assets/Scripts/StaffMembers/LoadFromText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Staff
+{
+    public class LoadFromText : MonoBehaviour
+    {
+        public List<string> m_Forname;
+        public List<string> m_SureName;
+        public List<string> m_Jobs;
+
+        // Use this for initialization
+        private void Awake()
+        {
+            
+        }
+        void Start()
+        {
+
+        }
+
+        string GetForeNames()
+        {
+            return m_Forname[Random.Range(0, m_Forname.Count)];
+        }
+
+    }
+}
