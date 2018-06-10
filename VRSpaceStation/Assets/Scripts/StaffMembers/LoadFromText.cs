05053555f502cb77a315f14569d5eedff7bf623f
Nickstu14 <nickstu14@hotmail.co.uk>
2018-06-10 08:41:07 +0000

    Trying to read in text files
    And slightly failing

--- VRSpaceStation/Assets/Scripts/StaffMembers/LoadFromText.cs
+++ VRSpaceStation/Assets/Scripts/StaffMembers/LoadFromText.cs
@@ -1,28 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace Staff
 {
     public class LoadFromText : MonoBehaviour
     {
-        public List<string> m_Forname;
+
+        public TextAsset [] s_TextFiles; // hardcoded 
+
+        public List<string> m_FornameFemale;
+        public List<string> m_FornameMale;
         public List<string> m_SureName;
         public List<string> m_Jobs;
+
+        
 
         // Use this for initialization
         private void Awake()
         {
-            
+            m_FornameFemale = new List<string>();
+            m_FornameMale = new List<string>();
+            m_SureName = new List<string>();
+            m_Jobs = new List<string>();
+            s_TextFiles = new TextAsset[4];
+
+            GetTextFileLocation();
+
         }
         void Start()
         {
 
         }
 
-        string GetForeNames()
+        void GetTextFileLocation()
         {
-            return m_Forname[Random.Range(0, m_Forname.Count)];
+            string path = "Scripts/StaffMembers/TextFiles/FemaleNames.txt";
+
+            //Read the text from directly from the test.txt file
+            s_TextFiles[0] = Resources.Load(path) as TextAsset;
+            print(s_TextFiles[0]);
+               // TextAsset bindata = Resources.Load("Texture") as TextAsset;
+
+        }
+
+        string GetForeNamesFemale()
+        {
+            return m_FornameFemale[Random.Range(0, m_FornameFemale.Count)];
         }
 
     }
