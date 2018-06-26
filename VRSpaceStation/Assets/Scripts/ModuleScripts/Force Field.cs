6510e7fe1a05e6194bff911e42578e57f63afecf
Amia196 <33600782+Amia196@users.noreply.github.com>
2018-06-26 19:18:13 +0100

    Modules Scripts

---
+++ VRSpaceStation/Assets/Scripts/ModuleScripts/Force Field.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Module
+{
+    public class ForceField : MonoBehaviour
+    {
+        private int m_ShieldStrength;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
+//Controls for strength of forcefield in engineering. Would this increase the difficulty
+//Strength of Forcefield 
---
+++ VRSpaceStation/Assets/Scripts/ModuleScripts/Research.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Module
+{
+    public class Research : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
