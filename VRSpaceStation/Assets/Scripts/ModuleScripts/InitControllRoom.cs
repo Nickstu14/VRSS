8534b37252b9e34d277f31303a7d9037f8155ca0
Nickstu14 <nickstu14@hotmail.co.uk>
2018-07-03 19:46:27 +0000

    Adding in object file structure

--- VRSpaceStation/Assets/Scripts/ModuleScripts/InitControllRoom.cs
+++ VRSpaceStation/Assets/Scripts/ModuleScripts/InitControllRoom.cs
@@ -13,7 +13,7 @@ namespace Module
             GameObject t_module = GameObject.Find("ModuleList").GetComponent<ModuleObjects>().GetControllRoom();
             t_module = Instantiate(t_module, transform.position, Quaternion.identity , gameObject.transform);
             t_module.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-            t_module.transform.Rotate(new Vector3(0, 0, 90));// = new Quaternion(90f, 0f, 0f,0f);
+            t_module.transform.Rotate(new Vector3(0, 0, 0));// = new Quaternion(90f, 0f, 0f,0f);
             t_module.GetComponent<Module.BasicModuleInfo>().SetMove(false);
 
 
